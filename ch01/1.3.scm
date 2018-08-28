ea379a5dd2b43ab92aaffe246360a96c4095dd16
gaufung <gaufung@outlook.com>
2018-08-28 19:31:17 +0800

    using jupyter notebook

--- ch01/1.3.scm
+++
@@ -1,8 +0,0 @@
-(define sum-of-two-from-three (a b c)
-    (if (> a b)
-        ((if (> b c)
-             (+ a b)
-             (+ a c)))
-        ((if (> a c)
-             (+ a b)
-             (+ b c)))))
